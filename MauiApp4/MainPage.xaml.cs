8fba3700f7666ea33141463f19f87a51cbd0bd67
Comlab <Comlab@CMLB2-3>
2023-10-16 08:25:46 +0800

    Add project files.

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	void OnVerticalStart(object sender, EventArgs e)
+	{
+		target.VerticalOptions = LayoutOptions.Start;
+	}
+    void OnVerticalCenter(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Center;
+    }
+    void OnVerticalEnd(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.End;
+    }
+    void OnHorizontalStart(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Start;
+    }
+    void OnHorizontalCenter(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Center;
+    }
+    void OnHorizontalEnd(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.End;
+    }
+}
+
